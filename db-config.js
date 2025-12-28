deb44d487bd5d064a3a37c394c77335ec1335ad5
mhocio <mikolajhoscilo@gmail.com>
2025-12-28 03:30:49 +0100

    Avoid Mongo index creation in tests

--- db-config.js
+++ db-config.js
@@ -2,12 +2,17 @@ const monk =  require('monk');
 
 const usersDb = monk(process.env.MONGODB_USERS_URI);
 const users = usersDb.get('users');
-users.createIndex({ name: 1 }, { unique: true });
-users.createIndex({ email: 1 }, { unique: true });
+const isTestEnv = process.env.NODE_ENV && String(process.env.NODE_ENV).toLowerCase() === 'test';
+if (!isTestEnv) {
+  users.createIndex({ name: 1 }, { unique: true });
+  users.createIndex({ email: 1 }, { unique: true });
+}
 
 const conversationsDb = monk(process.env.MONGODB_USERS_CONVERSATIONS);
 const conversations = conversationsDb.get('conversations');
-conversations.createIndex({ id: 1 }, { unique: true });
+if (!isTestEnv) {
+  conversations.createIndex({ id: 1 }, { unique: true });
+}
 
 async function closeDatabases() {
   await Promise.all([usersDb.close(), conversationsDb.close()]);
